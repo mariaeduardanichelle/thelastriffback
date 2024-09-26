cc0d4abd8e793d437d66699174ea707562fe4b6a
mariaeduardanichelle <madunichelle@gmail.com>
2024-09-26 14:19:12 -0300

    feat: cliente no pedido

--- thelastriff/models/pedido.py
+++ thelastriff/models/pedido.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+from django.conf import settings
+
 class Pedido(models.Model):
+    cliente = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     data_pedido = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Pedido {self.id}"
+        return f"Pedido {self.id} - Cliente: {self.cliente.username}"
+
