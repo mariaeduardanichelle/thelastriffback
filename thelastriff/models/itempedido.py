fb1884cca1263f9175a5c7fdcb2391c2bf197869
mariaeduardanichelle <madunichelle@gmail.com>
2024-09-26 15:05:34 -0300

    feat: localizacao

--- thelastriff/models/itempedido.py
+++ thelastriff/models/itempedido.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from django.utils.translation import gettext as _
+
 class ItemPedido(models.Model):
     pedido = models.ForeignKey('thelastriff.Pedido', on_delete=models.CASCADE)
     guitarra = models.ForeignKey('thelastriff.Guitarra', on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.quantidade}x {self.guitarra.modelo} no pedido {self.pedido.id}"
+        return _(f"{self.quantidade}x {self.guitarra.modelo} no pedido {self.pedido.id}")
     
--- thelastriff/models/pedido.py
+++ thelastriff/models/pedido.py
@@ -2,10 +2,12 @@ from django.db import models
 
 from django.conf import settings
 
+from django.utils.translation import gettext as _
+
 class Pedido(models.Model):
     cliente = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     data_pedido = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Pedido {self.id} - Cliente: {self.cliente.username}"
+        return _(f"Pedido {self.id} - Cliente: {self.cliente.username}")
 
