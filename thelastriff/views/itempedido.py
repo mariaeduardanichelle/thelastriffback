3cd87fdc5e5e4e2645b1265e780fbcb6ad75acf7
mariaeduardanichelle <madunichelle@gmail.com>
2024-09-26 14:24:35 -0300

    feat: configurando autenticacao

--- thelastriff/views/itempedido.py
+++ thelastriff/views/itempedido.py
@@ -3,6 +3,9 @@ from rest_framework.viewsets import ModelViewSet
 from thelastriff.models import ItemPedido
 from thelastriff.serializers import ItemPedidoSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
 class ItemPedidoViewSet(ModelViewSet):
     queryset = ItemPedido.objects.all()
     serializer_class = ItemPedidoSerializer
+    permission_classes = [IsAuthenticated]
--- thelastriff/views/pedido.py
+++ thelastriff/views/pedido.py
@@ -3,6 +3,9 @@ from rest_framework.viewsets import ModelViewSet
 from thelastriff.models import Pedido
 from thelastriff.serializers import PedidoSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
 class PedidoViewSet(ModelViewSet):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
+    permission_classes = [IsAuthenticated]
